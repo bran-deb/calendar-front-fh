9b21039e5813dc70441a0afab55a769a517ecce5
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-15 10:51:36 -0400

    added big-calendar

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
 import { Navbar } from '../ui/Navbar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+const events = [{
+    title: 'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    allDay: true,
+    bgcolor: '#fafafa',
+}]
 
 export const CalendarScreen = () => {
     return (
-        <div>
+        <div className='calendar-screen'>
             <Navbar />
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+            />
         </div>
     )
 }
