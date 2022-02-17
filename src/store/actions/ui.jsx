55deeddc4afbed640eb980d5bf511693181003ee
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-17 08:57:19 -0400

    ui actions

---
+++ src/store/actions/ui.jsx
@@ -0,0 +1,10 @@
+import { types } from "../types/types"
+
+
+export const uiOpenModal = () => ({
+    type: types.UI_OPEN_MODAL
+})
+
+export const uiCloseModal = () => ({
+    type: types.UI_CLOSE_MODAL
+})
