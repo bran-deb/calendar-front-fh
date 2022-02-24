68b9fdaff04d4d6938493568c372de96e29aa502
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-24 16:19:51 -0400

    added useForm & dispatch startLogin

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useForm } from '../../hooks/useForm'
+import { startLogin } from '../../store/actions/auth'
 import './login.css'
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch()
+
+    const [formLoginValues, handleLoginInputChange] = useForm({
+        lEmail: 'brandon@email.com',
+        lPassword: '123456',
+    })
+    const { lEmail, lPassword } = formLoginValues
+
+    //inicio session
+    const handleLogin = (e) => {
+        e.preventDefault()
+        dispatch(startLogin(lEmail, lPassword))
+    }
+
     return (
         <div className="container login-container">
             <div className="row">
                 <div className="col-md-6 login-form-1">
                     <h3>Ingreso</h3>
-                    <form>
+
+                    <form onSubmit={handleLogin}>
                         <div className="form-group">
                             <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Correo"
+                                name='lEmail'
+                                value={lEmail}
+                                onChange={handleLoginInputChange}
                             />
                         </div>
                         <div className="form-group">
@@ -20,6 +42,9 @@ export const LoginScreen = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
+                                name='lPassword'
+                                value={lPassword}
+                                onChange={handleLoginInputChange}
                             />
                         </div>
                         <div className="form-group">
@@ -30,10 +55,12 @@ export const LoginScreen = () => {
                             />
                         </div>
                     </form>
+
                 </div>
 
                 <div className="col-md-6 login-form-2">
                     <h3>Registro</h3>
+
                     <form>
                         <div className="form-group">
                             <input
@@ -72,6 +99,7 @@ export const LoginScreen = () => {
                                 value="Crear cuenta" />
                         </div>
                     </form>
+
                 </div>
             </div>
         </div>
---
+++ src/store/actions/auth.jsx
@@ -0,0 +1,8 @@
+
+
+//action async
+export const startLogin = (email, password) => {
+    return async (dispatch) => {
+        console.log(email, password);
+    }
+}
